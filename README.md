d0a11970d3551e8bddc66850d8e07df9820c3bfb
Leer0r <avedd72@gmail.com>
2020-10-12 16:14:53 +0200

    Add additionnal function

--- README.md
+++ README.md
@@ -9,6 +9,25 @@ from pyenv import env
 env = env()
 ```
 
-structure de env : 
-* content : contiens toutes les variables d'environement valides
+## Structure de env : 
+* content : Dictionnaire contenant les noms de variables et leurs valeurs
 * count : contiend le nombres de variables d'environement valide
+## Valeurs de retour possible
+* None : Le fichier .env n'a pas été trouvé
+* structure env : Au moins une valeur valide a été trouvée dans le fichier .env
+
+## Arguments annexe
+* location (str) : Il est possible de spécifier une localisation spéciale du fichier .env (pour les projet partagent le même fichier)
+  * utilisation : 
+  ```python
+  env = env(location=path_to_.env)
+  ```
+  * Valeur par default : "." (pwd)
+* error (bool) : Afficher les erreurs
+  * utilisation : 
+  ```
+  env = env(error=True)
+  ```
+  * Valeur par default : False
+
+
--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import os
 import re
 
-def env(location=".", error=False):
+def env(location:str =".", error:bool =False):
     if not os.path.exists(os.path.join(location,".env")):
-        if error:
-            print(f"File .env not fount at {location}")
         return None
     path = os.path.join(location,".env")
     content = {}
     count = 0
-    options = re.findall(r"([\w\.-]+)=([\w\.-]+)", open(path,"r").read())
-    if options:
-        for option in options:
-            content[option[0]] = option[1]
-            count += 1
-        return Env(content, count)
-    return None
+    p = re.compile(r"([\w\.-]+)=([\w\.-]+)")
+    with open(path,"r") as env:
+        line = env.readline()
+        while line != "":
+            res = p.match(line)
+            if res:
+                _split = res.group().split("=",1)
+                if error:
+                    if exist(content,_split[0]):
+                        print(f"Environement varable {_split[0]} already exist")
+                content[_split[0]] = _split[1]
+                count += 1
+            line = env.readline()
+    return Env(content, count)
     
+def exist(obj:object,key:str):
+    try:
+        obj[key]
+        return True
+    except KeyError:
+        return False
 
 class Env(object):
     def __init__(self,content,count):
         self.content = content
         self.count = count
+
+test = env(error=True)
+if test:
+    print(test.content)
